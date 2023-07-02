bc890d28b477e17fa2cfbc889ddbae98e5094921
Vasile Cristian <vasilecristian79@gmail.com>
2023-07-02 20:44:45 +0300

    02 iunie 20:44

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,33 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
+import SimpleLightbox from 'simplelightbox';
+
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+
 console.log(galleryItems);
+const gallery = document.querySelector('.gallery')
+
+galleryItems.forEach((item) => {
+  const galleryItem = document.createElement("li");
+  galleryItem.classList.add("gallery__item");
+
+  const galleryLink = document.createElement("a");
+  galleryLink.classList.add("gallery__link");
+  galleryLink.href = item.original;
+
+  const galleryImg = document.createElement("img");
+  galleryImg.classList.add("gallery__img");
+  galleryImg.src = item.preview;
+  galleryImg.setAttribute("data-source", item.original);
+  galleryImg.alt = item.description;
+
+  galleryItem.append(galleryLink)
+  galleryLink.append(galleryImg)
+  gallery.append(galleryItem)
+});
+
+  new SimpleLightbox('.gallery a', {
+    captionDelay: 250,
+});
