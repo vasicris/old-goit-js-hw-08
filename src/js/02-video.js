15279fe2d2c01d0ccb8db734e359d3cc33dfc310
Vasile Cristian <vasilecristian79@gmail.com>
2023-07-02 21:34:45 +0300

    02 iunie 21:34

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,28 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const player = new Player('vimeo-player');
+
+const savePlaybackTime = throttle(async () => {
+  try {
+    const currentTime = await player.getCurrentTime();
+    localStorage.setItem('videoplayer-current-time', currentTime);
+  } catch (error) {
+    console.error('Error saving playback time:', error);
+  }
+}, 1000);
+
+const resumePlayback = async () => {
+  try {
+    const currentTime = localStorage.getItem('videoplayer-current-time');
+    if (currentTime !== null) {
+      await player.setCurrentTime(parseFloat(currentTime));
+    }
+  } catch (error) {
+    console.error('Error resuming playback:', error);
+  }
+};
+
+player.on('timeupdate', savePlaybackTime);
+
+document.addEventListener('DOMContentLoaded', resumePlayback);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,39 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const emailInput = document.querySelector('input[name="email"]');
+const messageInput = document.querySelector('textarea[name="message"]');
+
+const updateLocalStorage = throttle(() => {
+  const formData = {
+    email: emailInput.value,
+    message: messageInput.value,
+  };
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+}, 500);
+
+const fillFormFields = () => {
+  const storedData = localStorage.getItem('feedback-form-state');
+  if (storedData) {
+    const { email, message } = JSON.parse(storedData);
+    emailInput.value = email;
+    messageInput.value = message;
+  }
+};
+
+const handleSubmit = (event) => {
+  event.preventDefault();
+  const formData = {
+    email: emailInput.value,
+    message: messageInput.value,
+  };
+  console.log(formData); 
+  localStorage.removeItem('feedback-form-state');
+  form.reset();
+};
+
+form.addEventListener('input', updateLocalStorage);
+
+document.addEventListener('DOMContentLoaded', fillFormFields);
+
+form.addEventListener('submit', handleSubmit)
